ea45cc1f6fee9410b5a0eda6a06499ef86e6079a
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2023-06-04 23:36:49 +0200

    dependency resolver part 2

---
+++ NVOS.Core/Services/ServiceDependencyResolver.cs
@@ -0,0 +1,138 @@
+﻿using NVOS.Core.Services.Attributes;
+using QuikGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVOS.Core.Services
+{
+    public class ServiceDependencyResolver
+    {
+        private BidirectionalGraph<Type, Edge<Type>> graph;
+        private List<Type> knownTypes;
+
+        public ServiceDependencyResolver()
+        {
+            graph = new BidirectionalGraph<Type, Edge<Type>>();
+            knownTypes = new List<Type>();
+        }
+
+        public ServiceDependencyResolver(params Type[] types)
+        {
+            graph = new BidirectionalGraph<Type, Edge<Type>>();
+            knownTypes = new List<Type>();
+            Register(types);
+        }
+
+        private IEnumerable<Type> GetRawDependencies(Type type)
+        {
+            foreach(ServiceDependencyAttribute attr in type.GetCustomAttributes<ServiceDependencyAttribute>())
+            {
+                if (attr.Type == type)
+                    throw new Exception($"Self-referential dependency detected: {attr.Type.Name}");
+
+                if (!attr.IsRequired)
+                    continue;
+
+                yield return attr.Type;
+            }
+        }
+
+        private void RecursiveResolveDependencies(List<Type> resolved, List<Type> unresolved, Type source)
+        {
+            unresolved.Add(source);
+            foreach (Edge<Type> edge in graph.OutEdges(source))
+            {
+                Type dependency = edge.Target;
+                if (!resolved.Contains(dependency))
+                {
+                    if (unresolved.Contains(dependency))
+                        throw new Exception($"Circular dependency detected: {source.Name} -> {dependency.Name}");
+
+                    RecursiveResolveDependencies(resolved, unresolved, dependency);
+                }
+            }
+
+            resolved.Add(source);
+            unresolved.Remove(source);
+        }
+
+        private void RecursiveResolveDependent(List<Type> resolved, List<Type> unresolved, Type source)
+        {
+            unresolved.Add(source);
+            foreach(Edge<Type> edge in graph.InEdges(source))
+            {
+                Type dependent = edge.Source;
+                if (!resolved.Contains(dependent))
+                {
+                    if (unresolved.Contains(dependent))
+                        throw new Exception($"Circular dependency detected: {dependent.Name} -> {source.Name}");
+
+                    RecursiveResolveDependent(resolved, unresolved, dependent);
+                }
+            }
+
+            resolved.Add(source);
+            unresolved.Remove(source);
+        }
+
+        private void BuildType(Type type)
+        {
+            foreach(Type dependency in GetRawDependencies(type))
+            {
+                graph.AddVerticesAndEdge(new Edge<Type>(type, dependency));
+
+                if (!knownTypes.Contains(dependency))
+                {
+                    knownTypes.Add(dependency);
+                    BuildType(dependency);
+                }
+            }
+        }
+
+        public void Rebuild()
+        {
+            graph.Clear();
+            foreach (Type type in knownTypes.ToArray())
+                BuildType(type);
+        }
+
+        public void Register(params Type[] types)
+        {
+            foreach(Type type in types)
+            {
+                if (!knownTypes.Contains(type))
+                    knownTypes.Add(type);
+            }
+
+            Rebuild();
+        }
+
+        public void Unregister(params Type[] types)
+        {
+            foreach (Type type in types)
+                knownTypes.Remove(type);
+
+            Rebuild();
+        }
+
+        public List<Type> ResolveDependencyOrder(Type type)
+        {
+            List<Type> resolved = new List<Type>();
+            List<Type> seen = new List<Type>();
+            RecursiveResolveDependencies(resolved, seen, type);
+            return resolved;
+        }
+
+        public List<Type> ResolveInverseDependencyOrder(Type type)
+        {
+            List<Type> resolved = new List<Type>();
+            List<Type> seen = new List<Type>();
+            RecursiveResolveDependent(resolved, seen, type);
+            return resolved;
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NVOS.Core.Database;
+using NVOS.Core.Services;
 using QuikGraph;
 using System;
 using System.Collections.Generic;
@@ -9,68 +10,17 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            BidirectionalGraph<int, Edge<int>> graph = new BidirectionalGraph<int, Edge<int>>();
-            graph.AddVertex(1);
-            graph.AddVertex(2);
-            graph.AddVertex(2);
-            graph.AddVertex(3);
-            graph.AddEdge(new Edge<int>(1, 2));
-            graph.AddEdge(new Edge<int>(1, 3));
-            foreach(Edge<int> e in graph.OutEdges(1))
-
-                Console.WriteLine(e);
-            Test test2 = new Test("cark", 12);
-
-            Console.WriteLine("Connection1 exists?: " + dbService.CollectionExists("collection1"));
-            Console.WriteLine("Connection2 exists?: " + dbService.CollectionExists("collection2"));
-            Console.WriteLine("Nonexistent collection exists?: " + dbService.CollectionExists("what"));
-
-            Console.WriteLine("Getting collection1");
-            Console.WriteLine(dbService.GetCollection("collection1"));
-            Console.WriteLine(dbService.GetCollection("collection2"));
-
-            Console.WriteLine("Getting test object");
-            ((Test)dbService.Read(collectionId2, "key1")).Sex();
-
-            Console.Read();
-
-            Console.WriteLine("Modifying collection1 key1 value");
-            dbService.Write(collectionId1, "key1", "booba");
-
-
-            Console.WriteLine("Writing to database (class objects)");
-            dbService.Write(collectionId2, "key1", test1);
-            dbService.Write(collectionId2, "key2", test2);
-            dbService.Write(collectionId2, "key3", test3);
-
-            Console.WriteLine("Reading from database (class objects)");
-            Console.WriteLine(dbService.Read(collectionId2, "key1"));
-            Console.WriteLine(dbService.Read(collectionId2, "key2"));
-            Console.WriteLine(dbService.Read(collectionId2, "key3"));
-
-            Console.WriteLine("Collection count: " + dbService.CountCollections());
-            //Console.WriteLine("Collection1 count: " + dbService.CountRecords(collectionId1));
-            Console.WriteLine("Collection2 count: " + dbService.CountRecords(collectionId2));
-
-            IEnumerable<string> collectionList = dbService.ListCollections();
-            Console.WriteLine("Listing collections:");
-            foreach (string collection in collectionList)
+            ServiceDependencyResolver graphResolver = new ServiceDependencyResolver(typeof(ServiceA), typeof(ServiceB), typeof(ServiceC), typeof(ServiceD), typeof(ServiceE), typeof(ServiceF));
+            Console.WriteLine("----------- ServiceA START CHAIN");
+            foreach(Type type in graphResolver.ResolveDependencyOrder(typeof(ServiceA)))
             {
-                Console.WriteLine(collection);
+                Console.WriteLine(type.Name);
             }
 
-            Console.WriteLine("Listing collection1 records:");
-            IEnumerable<KeyValuePair<string, object>> records1 = dbService.ListRecords(collectionId1);
-            foreach (KeyValuePair<string, object> record in records1)
+            Console.WriteLine("----------- ServiceC STOP CHAIN");
+            foreach (Type type in graphResolver.ResolveInverseDependencyOrder(typeof(ServiceC)))
             {
-                Console.WriteLine(record.Value);
-            }
-
-            Console.WriteLine("Listing collection2 records:");
-            IEnumerable<KeyValuePair<string, object>> records2 = dbService.ListRecords(collectionId2);
-            foreach (KeyValuePair<string, object> record in records2)
-            {
-                Console.WriteLine(message);
+                Console.WriteLine(type.Name);
             }
 
             Console.ReadLine();
