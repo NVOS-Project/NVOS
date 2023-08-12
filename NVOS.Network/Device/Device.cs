8faefe8b461d1186ef7fbf0a1082bc91e057c1b6
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2023-08-12 04:06:49 +0200

    implement RpcConnectionService

---
+++ NVOS.Network/Device/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVOS.Network.Device
+{
+    public class Device
+    {
+        public Guid Address { get; set; }
+        public List<int> Capabilities { get; set; }
+
+        public Device(Guid address, List<int> capabilities) 
+        {
+            Address = address;
+            Capabilities = capabilities;
+        }
+    }
+}
---
+++ NVOS.Network/Device/DeviceReflectionService.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Net.Client;
+using NVOS.Core;
+using NVOS.Core.Services;
+using NVOS.Core.Services.Attributes;
+using NVOS.Network.gRPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVOS.Network.Device
+{
+    [ServiceType(Core.Services.Enums.ServiceType.Singleton)]
+    [ServiceDependency(typeof(RpcConnectionService))]
+    public class DeviceReflectionService : IService
+    {
+        private RpcConnectionService rpcConnectionService;
+
+        private GrpcChannel channel;
+        private DeviceReflection.DeviceReflectionClient client;
+
+        public bool Init()
+        {
+            rpcConnectionService = ServiceLocator.Resolve<RpcConnectionService>();
+            channel = rpcConnectionService.GetChannel();
+            client = new DeviceReflection.DeviceReflectionClient(channel);
+
+            rpcConnectionService.ChannelConnected += RpcConnectionService_ChannelConnected;
+            rpcConnectionService.ChannelLost += RpcConnectionService_ChannelLost;
+            return true;
+        }
+
+        private void RpcConnectionService_ChannelConnected(object sender, EventArgs e)
+        {
+            channel = rpcConnectionService.GetChannel();
+            client = new DeviceReflection.DeviceReflectionClient(channel);
+        }
+
+        private void RpcConnectionService_ChannelLost(object sender, EventArgs e)
+        {
+            channel = null;
+            client = null;
+        }
+
+
+    }
+}
