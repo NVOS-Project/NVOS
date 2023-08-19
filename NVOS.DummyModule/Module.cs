c3519440fe716bbde47bf2bceda3b59b214083e0
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2023-08-19 13:10:51 +0200

    Implement dummy unity test services

---
+++ NVOS.DummyModule/Module.cs
@@ -0,0 +1,21 @@
+﻿using NVOS.Core.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVOS.DummyModule
+{
+    public class Module : IModule
+    {
+        public string Name => "DummyModule";
+
+        public string Description => "Module implementing services that spawn some Unity objects";
+
+        public string Author => "NVOS Project";
+
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
---
+++ NVOS.DummyModule/SphereService.cs
@@ -0,0 +1,34 @@
+﻿using NVOS.Core;
+using NVOS.Core.Services.Attributes;
+using NVOS.Core.Services.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NVOS.DummyModule
+{
+    [ServiceType(ServiceType.Singleton)]
+    public class SphereService : IService, IDisposable
+    {
+        private GameObject gameObject;
+
+        public bool Init()
+        {
+            gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            gameObject.transform.position = new Vector3(0, 0, 5);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            if (gameObject == null)
+                return;
+
+            GameObject.Destroy(gameObject);
+            gameObject = null;
+        }
+    }
+}
