6619b02043d542bc963cdc0105c02b02def3b485
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2023-08-21 22:56:52 +0200

    Add missing module manifest to NVOS.Unity

---
+++ NVOS.Unity/Module.cs
@@ -0,0 +1,19 @@
+﻿using NVOS.Core.Modules;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace NVOS.Unity
+{
+    public class Module : IModule
+    {
+        public string Name => "NVOS.Unity";
+
+        public string Description => "A common set of Unity utilities";
+
+        public string Author => "NVOS Project";
+
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
