ea96b5dc1b8c175ecb1f46f32a2c9b9eb0493a22
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2023-08-24 18:17:43 +0200

    Fix 3D window relocation

--- NVOS.UI/Services/WorldUIService.cs
+++ NVOS.UI/Services/WorldUIService.cs
@@ -25,8 +25,8 @@ namespace NVOS.UI.Services
         private bool isMoving;
         private float moveWaitTime;
         private float moveWaitTimePassed;
-        private float windowSpeed;
-        private float rotationSpeed;
+        private float positionSmoothing;
+        private float rotationSmoothing;
         private float walkSpeed;
         private float windowSpawnDistance;
         private float windowBubbleRadius;
@@ -37,17 +37,17 @@ namespace NVOS.UI.Services
             IDatabaseService db = ServiceLocator.Resolve<IDatabaseService>();
             DbCollection collection = db.GetCollection("world_ui");
 
-            moveWaitTime = (float)collection.ReadOrDefault("moveWaitTime", 2.5f); 
-            windowSpeed = (float)collection.ReadOrDefault("windowSpeed", 2f); // 2 meters per second
-            rotationSpeed = (float)collection.ReadOrDefault("rotationSpeed", 360f); // 360 degrees per second
-            walkSpeed = (float)collection.ReadOrDefault("walkSpeed", 0.7f);
-            windowSpawnDistance = (float)collection.ReadOrDefault("windowSpawnDistance", 0.5f);
+            moveWaitTime = (float)collection.ReadOrDefault("moveWaitTime", 1.25f); 
+            positionSmoothing = (float)collection.ReadOrDefault("positionSmoothing", 0.125f);
+            rotationSmoothing = (float)collection.ReadOrDefault("rotationSmoothing", 0.125f);
+            walkSpeed = (float)collection.ReadOrDefault("walkSpeed", 0.1f);
+            windowSpawnDistance = (float)collection.ReadOrDefault("windowSpawnDistance", 0.25f);
             windowBubbleRadius = (float)collection.ReadOrDefault("windowBubbleRadius", 1f);
 
             worldAnchor = new GameObject("WorldUIAnchor");
             Transform cameraTransform = Camera.main.transform;
             worldAnchor.transform.position = cameraTransform.position;
-            worldAnchor.transform.rotation = Quaternion.Euler(0f, cameraTransform.rotation.eulerAngles.y, 0f);
+            worldAnchor.transform.eulerAngles = new Vector3(0f, cameraTransform.rotation.eulerAngles.y, 0f);
 
             windows = new List<Window3D>();
             updateProvider.OnLateUpdate += UpdateProvider_OnLateUpdate;
@@ -80,15 +80,14 @@ namespace NVOS.UI.Services
             GameObject windowObject = window.GetRootObject();
             windowObject.transform.SetParent(worldAnchor.transform, true);
 
-            Vector3 windowPosition = (cameraTransform.forward * windowSpawnDistance) + cameraTransform.position;
-            windowObject.transform.position = windowPosition;
+            Vector3 targetWorldPos = (cameraTransform.forward * windowSpawnDistance) + cameraTransform.position;
+            windowObject.transform.localPosition = windowObject.transform.InverseTransformPoint(targetWorldPos);
 
-            Vector3 directionVector = (windowObject.transform.position - cameraTransform.position).normalized;
-            windowObject.transform.rotation = Quaternion.LookRotation(directionVector, Vector3.up);
+            Vector3 directionVector = windowObject.transform.localPosition.normalized;
+            windowObject.transform.localRotation = Quaternion.LookRotation(directionVector, Vector3.up);
 
             windows.Add(window);
             window.OnClose += Window_OnClose;
-            window.OnWindowStateChanged += Window_OnWindowStateChanged;
 
             return window;
         }
@@ -118,22 +117,16 @@ namespace NVOS.UI.Services
                 window.Update();
 
                 GameObject windowObject = window.GetRootObject();
-                Vector3 windowPosition = windowObject.transform.position;
-
-                float distance = Vector3.Distance(windowPosition, worldAnchorTransform.position);
-
-                if (distance > windowBubbleRadius)
-                {
-                    Vector3 vector = (windowPosition - worldAnchorTransform.position).normalized * windowBubbleRadius;
-                    windowObject.transform.position = vector;
-                }
-
-                windowObject.transform.eulerAngles = new Vector3(0f, windowObject.transform.eulerAngles.y, 0f);
+                // Constrain the window position to the bubble
+                windowObject.transform.localPosition = Vector3.ClampMagnitude(windowObject.transform.localPosition, windowBubbleRadius);
+                // Constain the rotation to look at the anchor
+                Quaternion rotation = Quaternion.LookRotation(windowObject.transform.localPosition.normalized, Vector3.up);
+                windowObject.transform.localEulerAngles = new Vector3(0f, rotation.eulerAngles.y, 0f);
             }
 
             if (!isMoving)
             {
-                if (Vector3.Distance(cameraTransform.position, worldAnchorTransform.position) > 3f)
+                if (Vector3.Distance(cameraTransform.position, worldAnchorTransform.position) > windowBubbleRadius)
                 {
                     isMoving = true;
                     moveWaitTimePassed = 0f;
@@ -148,12 +141,12 @@ namespace NVOS.UI.Services
 
             if (isMoving)
             {
-                float position_step = windowSpeed * Time.deltaTime;
-                float rotation_step = rotationSpeed * Time.deltaTime;
-                worldAnchorTransform.position = Vector3.MoveTowards(worldAnchorTransform.position, cameraTransform.position, position_step);
-                worldAnchorTransform.rotation = Quaternion.RotateTowards(worldAnchorTransform.rotation, Quaternion.Euler(0f, worldAnchorTransform.rotation.eulerAngles.y, 0f), rotation_step);
+                float position_step = Mathf.Clamp01(Mathf.Max(1f - positionSmoothing, 0.01f) * 10 * Time.deltaTime);
+                float rotation_step = Mathf.Clamp01(Mathf.Max(1f - rotationSmoothing, 0.01f) * 10 * Time.deltaTime);
+                worldAnchorTransform.position = Vector3.Lerp(worldAnchorTransform.position, cameraTransform.position, position_step);
+                worldAnchorTransform.rotation = Quaternion.Lerp(worldAnchorTransform.rotation, Quaternion.Euler(0f, cameraTransform.rotation.eulerAngles.y, 0f), rotation_step);
 
-                if (Vector3.Distance(worldAnchorTransform.position, cameraTransform.position) < 1f)
+                if (Vector3.Distance(worldAnchorTransform.position, cameraTransform.position) < 0.01f)
                 {
                     if (Camera.main.velocity.magnitude < walkSpeed)
                         moveWaitTimePassed += Time.deltaTime;
@@ -172,23 +165,6 @@ namespace NVOS.UI.Services
                             window.GetRootObject().SetActive(true);
                     }
                 }
-            }
-        }
-
-        private void Window_OnWindowStateChanged(object sender, WindowStateChangedEventArgs e)
-        {
-            if (e.State == Models.Enums.WindowState.Normal)
-            {
-                Window3D window = (Window3D)e.Window;
-
-                Transform cameraTransform = Camera.main.transform;
-                GameObject windowObject = window.GetRootObject();
-
-                Vector3 windowPosition = (cameraTransform.forward * windowSpawnDistance) + cameraTransform.position;
-                windowObject.transform.position = windowPosition;
-
-                Vector3 directionVector = (windowObject.transform.position - cameraTransform.position).normalized;
-                windowObject.transform.rotation = Quaternion.LookRotation(directionVector, Vector3.up);
             }
         }
     }
